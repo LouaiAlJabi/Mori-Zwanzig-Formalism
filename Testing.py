0b9eb95cd108cce0786dacd222ee54596808e9b2
Lou <88289342+LouaiAlJabi@users.noreply.github.com>
2024-06-10 15:29:41 -0400

    .

--- Testing.py
+++ Testing.py
@@ -1,12 +1,6 @@
-import Lou_MoriZwanzig as mz
+import Lou_Bootstrap as lbs
 import time as tm
+import Lou_MoriZwanzig as lmz
+import numpy as np
 
-if __name__ == '__main__':
-    start = tm.time()
-    velocity, force, sq_displacement = mz.LoadDatafile("velocities.npy", "forces.npy", "sq_displacement_stat.npy")
-    part_mass, dt,KB,T,Omega = mz.VarSetter(80.,0.005,1.,1.,0)
-    list_, Ein_list = mz.main(16,4,1,part_mass,dt,KB,T,Omega,velocity,force,sq_displacement) 
-    result = mz.Fixup(list_,Ein_list)
-    result
-    print(' The bootstrapping finished in:\n',round(((tm.time()-start)/60 / 60),2) ,'hours')
-
+dataSet = np.random.randint(25)
